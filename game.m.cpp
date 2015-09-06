d483d83e690cc45b5c8c66b36296da652317269d
sunny <sunnyding602@qq.com>
2015-09-06 00:18:05 -0400

    finish writing cmake

--- game.m.cpp
+++ game.m.cpp
@@ -1,8 +1,11 @@
 //main file
 #include<iostream>
 using namespace std;
+#include"myclass.h"
 
 int main(){
+    MyClass mc;
+    mc.sayHello(1);
     cout << "Hello world!" <<endl; 
     return 0;
 }
